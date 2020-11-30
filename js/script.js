13ddc44718b7497524de30921607c13f060de0d6
UglanovEA <uglanovea@gmail.com>
2020-11-30 16:33:53 +0300

    ver 1.8 add mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $("#carouselOne").owlCarousel({
     items: 1,
     nav: false,
     loop: true
   });
+  $("#carouselTwo").owlCarousel({
+    items: 4,
+    margin: 15,
+    nav: false,
+    dots: false,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      650: {
+        items: 3
+      },
+      1024: {
+        items: 4
+      }
+    }
+  });
+  var owl = $('#carouselTwo');
+  owl.owlCarousel();
+  $('.portfolio-controls__arrow-next').click(function () {
+    owl.trigger('next.owl.carousel');
+  })
+  $('.portfolio-controls__arrow-prev').click(function () {
+    owl.trigger('prev.owl.carousel', [300]);
+  })
 });
